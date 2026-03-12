f80e5d1b19ad50efe7c7cbb8c6a4d6a78060a1f0
PLUCK-Ziphen <akhilesheerni@gmail.com>
2026-03-12 09:24:28 +0530

    UC1: Application Entry & Welcome Message

--- BookMyStayApp/src/HotelBookingApp.java
+++ BookMyStayApp/src/HotelBookingApp.java
@@ -1,3 +1,15 @@
 public class HotelBookingApp {
+    public static void main(String[] args) {
 
+        // Displaying the application welcome message
+        System.out.println("Welcome to the Hotel Booking Management System");
+
+        // Displaying application metadata
+        System.out.println("Application Name: Book My Stay");
+        System.out.println("Version: 1.0");
+
+        // Confirmation message
+        System.out.println("System has started successfully.");
+
+    }
 }
